2951eb55d6335b31e7b28eb81b7c588901404065
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-01-12 12:26:36 -0500

    finish frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store/store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+
+
+const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  window.store = store;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+   </Provider>
+  </React.StrictMode>
+);
