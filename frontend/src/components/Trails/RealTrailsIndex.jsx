6489dfed52f30942c492dedf96d0d6ac794e8f95
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-01-17 22:03:12 -0500

    add sidebar

--- frontend/src/components/Trails/RealTrailsIndex.jsx
+++ frontend/src/components/Trails/RealTrailsIndex.jsx
@@ -1,34 +1,47 @@
-import { useDispatch, useSelector } from "react-redux"
-import { Fetchtrails } from "../../store/trail"
-import { useEffect } from "react"
-import TrailsIndexItem from "./TrailsIndexItem"
-import { trailsArray } from "../../store/trail"
-import "./RealTrailsIndex.css"
+import { useDispatch, useSelector } from "react-redux";
+import { Fetchtrails } from "../../store/trail";
+import { useEffect, useState } from "react";
+import TrailsIndexItem from "./TrailsIndexItem";
+import { trailsArray } from "../../store/trail";
+import "./RealTrailsIndex.css";
 
-function RealTrailsIndex(){
-    const trails = useSelector(trailsArray)
-    console.log(trails);
-        
-        
+function RealTrailsIndex() {
+  const trails = useSelector(trailsArray);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-    useEffect(() => {
-       dispatch(Fetchtrails())
-    }, [dispatch]);
-    return(
-        <div>
-        <div id='realindexWrapper'>
-            <br />
-            <h1 id="RealTrailIndexHeader">New York Favorites</h1>
-            <br />
-        <ul id="realtrailindexwrapper">
-            {trails.map(trail => 
-                <TrailsIndexItem key={trail.id} trail={trail} />)}
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    dispatch(Fetchtrails());
+  }, [dispatch]);
+
+  return (
+    <div>
+     <div id="realTrailIndexHeader"></div>
+      <div id="realindexWrapper" className={isSidebarOpen ? "sidebaropen" : ""}>
+        <ul id="openBtn" onClick={toggleSidebar}>
+           
+          &#9776; 
         </ul>
-            
+      
+        <div id="sidebar" className={isSidebarOpen ? "open" : ""}>
+            <div id="sidebarHeader"> 
+                <h1>New York Best Trails</h1>
+            </div>
+          <ul>
+            {trails.map((trail) => (
+              <TrailsIndexItem key={trail.id} trail={trail} />
+            ))}
+          </ul>
         </div>
-        </div>
-    )
+
+      </div>
+    </div>
+  );
 }
-export default RealTrailsIndex
+
+export default RealTrailsIndex;
