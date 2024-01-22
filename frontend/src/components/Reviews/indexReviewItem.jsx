9e575d79fb6c732d29b169c8a50d2bdb7bd24b9d
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-01-22 16:28:05 -0500

    Format index rating

--- frontend/src/components/Reviews/indexReviewItem.jsx
+++ frontend/src/components/Reviews/indexReviewItem.jsx
@@ -1,8 +1,23 @@
-
+import "./indexReviewItem.css"
+import Rating from "./rating"
+import { formatDate } from "./formatedate"
+import reviewProfile from "../../images/reviewProfile.png"
 function ReviewIndexItem({review}) {
+    const capitalizeFirstLetter = (str) =>  {
+        return str[0].toUpperCase() + str.slice(1);
+      }
     return (
         <div id="reviewsContent">
-        <h1>{review.review}</h1>
+            <hr />
+            <span id="reviewIndexItemName"> <img src={reviewProfile} alt="reviewProfile" id="reviewProfileImag"/>
+            <div>
+            {capitalizeFirstLetter(review.fname)} {capitalizeFirstLetter(review.lname)} 
+        
+            <p id="reviewDateCreated">{formatDate(review.created_at)}</p>
+            </div>
+            </span>
+            <Rating rating={review.rating}/>
+            <p id="reviewIndexItemDescription">{review.review}</p>
         </div>
     )
 }
