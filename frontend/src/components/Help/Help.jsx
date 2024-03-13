b7f70a224076838140d7f463b4276f9f6f0580ee
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-03-13 11:29:55 -0400

    Work on styling for help page

--- frontend/src/components/Help/Help.jsx
+++ frontend/src/components/Help/Help.jsx
@@ -17,9 +17,9 @@ function Help () {
         <br />
         <p id="Statement">  The collapsible sidebar on the left contains all the trails currently reviewed and available on our site. When you hover over a trail, it will zoom in on its location and you can get a feel for what the trail looks like. Clicking on a trail allows you to see more details and to view, create, edit, or delete reviews and ratings</p>
         <br />
-        <p id="Statement">  At any time, you can click on the &quot;All Adventure&quot; logo to return to the home page. To access the maps again, simply click on &quot;Explore Nearby Trails&quot; and you wll be taken back to the map. You can also search for a particular trail by name using the search bar</p>
+        <p id="Statement">  At any time, you can click on the &quot;All Adventure&quot; logo to return to the home page. To access the maps again, simply click on &quot;Explore Nearby Trails&quot; or &quot;New York Favorites&quot; and you wll be taken back to the map. You can also search for a particular trail by name or search by difficulty of trails using the search bar.</p>
         <br />
-        <p id="Statement">  If you wish to leave the site, hover over the profile icon where you wll find a dropdown menu allowing you to log out.</p>
+        <p id="Statement1">  If you wish to leave the site, hover over the profile icon where you wll find a dropdown menu allowing you to log out.</p>
         <br />
         
     
