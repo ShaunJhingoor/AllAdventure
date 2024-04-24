1e5b3ea0ed76883467f5dac7d9d376a020c30914
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-04-24 00:01:04 -0400

    footer

--- frontend/src/components/search/searchIndexItem.jsx
+++ frontend/src/components/search/searchIndexItem.jsx
@@ -1,4 +1,3 @@
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import AverageRating from '../Rating/averagerating'
 
@@ -6,34 +5,24 @@ import '../search/SearchIndexItem.css'
 
 
 function SearchIndexItem({result}){
-    const currentUser = useSelector(state=> state?.session.user)
     
     
     return (
-        
         <>
-      <div id='searchInfo'>
-
-    <Link to={ currentUser? `/trails/${result.id}`: '/signUp'} >
-        <img src={result?.photoUrl} alt="result" id="searchImag" />
-    </Link>
-    <Link to={currentUser? `/trails/${result.id}`: '/signUp'} style={{ textDecoration: 'none' }}>
-
-        <div className="detailsContainer">
-            <p id='searchtrailname'>{result.name}</p>
-            <ul id='searchTrail'> 
-                <li>&#9733; <AverageRating trail={result}/> &bull; {result.length}mi &bull; {result.difficulty}</li>
-            </ul>
+        <div id='searchInfo'>
+            <Link to={`/trails/${result.id}`} style={{ textDecoration: 'none' }}>
+                <img src={result?.photoUrl} alt="result" id="searchImag" />
+                <div className="detailsContainer">
+                    <p id='searchtrailname'>{result.name}</p>
+                    <ul id='searchTrail'> 
+                        <li>&#9733; <AverageRating trail={result}/> &bull; {result.length}mi &bull; {result.difficulty}</li>
+                    </ul>
+                </div>
+            </Link>
         </div>
-    </Link>
-</div>
-
-            
-            
-        
-        </>   
-        
-        
-    )
+    </>
+)
 }
+        
+        
 export default SearchIndexItem
